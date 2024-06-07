d1a2249306f24a1fe45142bb54c4edf31e79241f
osee mbiya <om346@live.mdx.ac.uk>
2024-06-07 12:30:24 +0100

    Portfolio in progress

---
+++ Custom.js
@@ -0,0 +1,8 @@
+// Theme light mode
+document.getElementById("Light").addEventListener("click", function () {
+  document.body.style.backgroundColor = "#ffffff";
+});
+// Theme dark mode
+document.getElementById("Dark").addEventListener("click", function () {
+  document.body.style.backgroundColor = "#000000";
+});
